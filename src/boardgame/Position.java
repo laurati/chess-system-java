9269664675ee1724caa63c7a45dc6cbba530338b
laurati <lauraecomarquez@gmail.com>
2019-11-26 08:45:35 -0300

    First class: Position

---
+++ src/boardgame/Position.java
@@ -0,0 +1,44 @@
+package boardgame;
+
+public class Position {
+	
+	private int row; //linha
+	private int column; //coluna
+	
+	
+	public Position(int row, int column) {
+		this.row = row;
+		this.column = column;
+	}
+
+
+	public int getRow() {
+		return row;
+	}
+
+
+	public int getColumn() {
+		return column;
+	}
+
+
+	public void setRow(int row) {
+		this.row = row;
+	}
+
+
+	public void setColumn(int column) {
+		this.column = column;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return row + ", " + column;
+	}
+	
+	
+	
+	
+
+}
